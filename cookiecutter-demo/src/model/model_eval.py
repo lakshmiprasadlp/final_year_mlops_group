d03bab5823783f55a9ee3d12311997030802db69
lakshmiprasadlp <145165461+lakshmiprasadlp@users.noreply.github.com>
2025-05-02 20:20:42 +0530

    Add files via upload

---
+++ cookiecutter-demo/src/model/model_eval.py
@@ -0,0 +1,73 @@
+import os
+import numpy as np
+import pandas as pd
+import pickle
+import json
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+def load_data(filepath: str) -> pd.DataFrame:
+    try:
+        return pd.read_csv(filepath)
+    except Exception as e:
+        raise Exception(f"Error loading data from {filepath}: {e}")
+
+def prepare_data(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
+    try:
+        X = data.drop(columns=['Potability'], axis=1)
+        y = data['Potability']
+        return X, y
+    except Exception as e:
+        raise Exception(f"Error preparing data: {e}")
+
+def load_model(filepath: str):
+    try:
+        with open(filepath, "rb") as file:
+            model = pickle.load(file)
+        return model
+    except Exception as e:
+        raise Exception(f"Error loading model from {filepath}: {e}")
+
+def evaluate_model(model, X_test: pd.DataFrame, y_test: pd.Series) -> dict:
+    try:
+        y_pred = model.predict(X_test)
+        
+        metrics_dict = {
+            'accuracy': accuracy_score(y_test, y_pred),
+            'precision': precision_score(y_test, y_pred),
+            'recall': recall_score(y_test, y_pred),
+            'f1_score': f1_score(y_test, y_pred)
+        }
+        return metrics_dict
+    except Exception as e:
+        raise Exception(f"Error evaluating model: {e}")
+
+
+
+def save_metrics(metrics: dict, metrics_path: str) -> None:
+    try:
+        # Ensure the directory exists
+        os.makedirs(os.path.dirname(metrics_path), exist_ok=True)
+        
+        with open(metrics_path, 'w') as file:
+            json.dump(metrics, file, indent=4)
+    except Exception as e:
+        raise Exception(f"Error saving metrics to {metrics_path}: {e}")
+
+def main():
+    try:
+        test_data_path = "./data/processed/test_processed_mean.csv"
+        model_path = "models/model.pkl"
+        metrics_path = "reports/metrics.json"
+        
+        test_data = load_data(test_data_path)
+        X_test, y_test = prepare_data(test_data)
+        model = load_model(model_path)
+        metrics = evaluate_model(model, X_test, y_test)
+        save_metrics(metrics, metrics_path)
+        
+        print("Model evaluation completed successfully. Metrics saved to", metrics_path)
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
